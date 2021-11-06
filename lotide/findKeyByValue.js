e6335989a69f0a8bcf58224f394f6f90ad1bd0d0
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-06 11:03:32 +0000

    first load findKeyByValue but values & keys of object not picked, hence returning undefined.

--- lotide/findKeyByValue.js
+++ lotide/findKeyByValue.js
@@ -9,19 +9,37 @@ const assertEqual = function(actual, expected) {
     // print
 };
 
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
 const findKeyByValue = function ({object}, value) {
-    object.key()...........
+  // console.log(Object.values(bestTVShowsByGenre));
+  // console.log(Object.values(object));
+  // console.log(Object.keys(bestTVShowsByGenre));
+  //console.log(Object.keys(object));
+   for (let obj in object) { //The value of object is not being picked by this code
+    console.log(object.obj);
+     if (value === Object.values(object)) {
+      return Object.keys(object)
+    }
+  }
+  
 }
 
 
 
 
 // Test cases:
-const bestTVShowsByGenre = { 
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama:  "The Wire"
-  };
+// const bestTVShowsByGenre = { 
+//     sci_fi: "The Expanse",
+//     comedy: "Brooklyn Nine-Nine",
+//     drama:  "The Wire"
+//   };
+  console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
   
   assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "drama");
   assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
