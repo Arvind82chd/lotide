057bf33c90d512ba0e1c80855200e8d48f6a0603
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-06 19:48:01 +0000

    first load map.js - issue with eqArray hence nnot compatible with assertarray hence eqArray needs corrections

---
+++ d4-funcs/map_builtin/mapTest.js
@@ -0,0 +1,7 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+const map = lighthouses.map(function () {for (i = 0; i < lighthouses.indexOf; i++) {
+  return lighthouses[i].length;
+}});
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
+console.log(map);
--- lotide/map.js
+++ lotide/map.js
@@ -1,5 +1,5 @@
 const words = ["ground", "control", "to", "major", "tom"];
-const map = function(array, callback) {
+const map = function(array, callback) { //can't remmember when I did this.
    const results = [];
    for (let item of array) {
       results.push(callback(item));
@@ -7,6 +7,31 @@ const map = function(array, callback) {
    return results;
 } 
 
+const assertArrayEqual = function(array1, array2) {
+   if (eqArrays(array1, array2)) {
+     console.log(`🟢🟢🟢 Assertion Passed: [actual] === [expected]`);
+   } else {
+     console.log(`🔴🔴🔴 Assertion Failed: [actual] !== [expected]`);
+   }
+   //define two inputs
+   //then compare
+   // print
+ }; 
+
+const eqArrays = function(array1, array2) {
+if (array1.length !== array2.length) { 
+   return false;
+} else
+   for (let i = 0; i < array1.length; i++) {
+   if (array1[i] !== array2[i]) {
+      return false;
+   } 
+   else
+   return true;
+   }
+};
 
 const results = map(words, word => word[0]);
 console.log(results);
+//assertArrayEqual(results);
+
