d8808070e200e5c9dacc5279f5b896f1ab55969e
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-06 20:17:27 +0000

    letterPositions.js first load in working condition after guidance

--- lotide/letterPositions.js
+++ lotide/letterPositions.js
@@ -1,57 +1,23 @@
 const sentence = "This is a good day today.";
 const letterPositions = function(sentence) {
-const results = {};
-let key = "";
-for (let letter of sentence) {
-  if (letter !== " ") {
-  for (i = 0; i < letter.length; i++) {
-    key = letter[i]; 
-        //  return i;
-  }
- }
-    //console.log();
-    
-    //results[letter] = [firstindex]
-   // results[letter].push(letter);
-    console.log(results[letter]); 
-} 
-
-return results;   
-};
-// for (let letter of string) {
-//     if (letter !== " ") {
-//       if (countObject[letter]) { // condition to check if letter repeated.
-//         countObject[letter] += 1;
-//       }
-//       else 
-//       {
-//         countObject[letter] = 1;
-//       }
-//     }
-//   }
-//   return countObject 
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) { 
-  return false;
-  } else
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-      return false;
-      } 
-      else
-      return true;
+  const results = {};
+  for (let l = 0; l <= sentence.length; l++){
+      
+    if(sentence[l]!==" "){ //This if condition is to not to evaluate the blank spaces 
+      if(results[sentence[l]]){ //sentence[l] = "a"
+        results[sentence[l]].push(l); // for sending repeating values to results{}
+      } else {
+        results[sentence[l]] = [l]; // for sending first value to results{}
+      }
     }
+   } 
+  return results;   
 };
 
-const assertArrayEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`🟢🟢🟢 Assertion Passed: [actual] === [expected]`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: [actual] !== [expected]`);
-  }
 
-}; 
+// const assertObjects  = function(passed, expected){
+  
+// }
     
 console.log(letterPositions(sentence));
-//assertArraysEqual(letterPositions("hello"), [1]);
+//assertArraysEqual(letterPositions("hello"), {h:[0], e:[1], l:[2, 3], o: [4]});
