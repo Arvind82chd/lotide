c907fcb8c89409a9fde65be01a2288f1eeeb55d2
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-06 09:18:29 +0000

    assertObjectsEqual file first upload after completion.

---
+++ lotide/assertObjectsEqual.js
@@ -0,0 +1,11 @@
+const assertObjectsEqual = function(actual, expected) {
+    const inspect = require('util').inspect;
+    if (eqObjects(actual, expected)) {
+        console.log(`🟢🟢🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+    } else {
+        console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    }
+    //define two inputs
+    //then compare
+    // print
+};
