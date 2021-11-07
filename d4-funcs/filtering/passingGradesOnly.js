6ed2dd5d394097d31ea079d136ea155bcc3a8e76
Arvind Sharma <mail.arvind.s@gmail.com>
2021-11-07 08:11:17 +0000

    corrected output for passing grades

--- d4-funcs/filtering/passingGradesOnly.js
+++ d4-funcs/filtering/passingGradesOnly.js
@@ -1,5 +1,5 @@
 const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
 
-const pass = function(Passed) {
-    Passed = grades.filter( grade > 70 )
-}
+const pass = grades.filter(grade => grade > 70);
+
+console.log(pass);
